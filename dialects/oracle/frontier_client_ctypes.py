0b7d3a5e3b518982d3082f65d7f19672014cd688
Zhen Xie <zhen@pczhen.cern.ch>
2016-11-07 15:09:59 +0100

    add lib suffix select, add missing argtypes

--- dialects/oracle/frontier_client_ctypes.py
+++ dialects/oracle/frontier_client_ctypes.py
@@ -1,10 +1,18 @@
-import os.path
+import os
 import ctypes
 import logging
 
+libsuffix = None
+if os.name == "posix" and sys.platform == "darwin":
+    libsuffix = ".dylib"
+elif os.name == "posix":
+    libsuffix = ".so"
+else:
+    raise ValueError("Unsupported platform %s"%sys.platform)
+
 me = os.path.abspath(os.path.dirname(__file__))
-#libfc = ctypes.cdll.LoadLibrary(os.path.join(me,'..','libfrontier_client.so.2'))
-libfc = ctypes.cdll.LoadLibrary(os.path.join(me,'..','..','..','..','..','libfrontier_client.so'))
+libfcname = "libfrontier_client"+libsuffix
+libfc = ctypes.CDLL(os.path.join(me,'..','..','..','..','..',libfcname),mode=ctypes.RTLD_GLOBAL)
 logger = logging.getLogger(__name__)
 
 #wrapped header files: frontier_error.h, frontier.h, frontier-cpp.h
@@ -37,6 +45,128 @@ FRONTIER_MAX_SERVERN = 16
 FRONTIER_MAX_PROXYN = 24
 FRONTIER_MAX_PROXYCONFIGN = 8
 
+#functions accept any ctypes data instances as arguments and return the default result type specified by the library loader.
+
+# frontier_error.h frontier_getErrorMsg
+frontier_getErrorMsg = libfc.frontier_getErrorMsg
+frontier_getErrorMsg.restype = ctypes.c_char_p 
+
+#frontier.h frontierRSBlob_open
+frontierRSBlob_open = libfc.frontierRSBlob_open
+frontierRSBlob_open.restype = ctypes.c_void_p
+frontierRSBlob_open.argtypes = [ctypes.c_ulong, ctypes.c_void_p, ctypes.c_int, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_close
+frontierRSBlob_close = libfc.frontierRSBlob_close
+frontierRSBlob_close.restype = None
+frontierRSBlob_close.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_start
+frontierRSBlob_start = libfc.frontierRSBlob_start
+frontierRSBlob_start.restype = None
+frontierRSBlob_start.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+
+#frontier.h frontierRSBlob_getByte
+frontierRSBlob_getByte = libfc.frontierRSBlob_getByte
+frontierRSBlob_getByte.restype = ctypes.c_byte
+frontierRSBlob_getByte.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_checkByte
+frontierRSBlob_checkByte = libfc.frontierRSBlob_checkByte
+frontierRSBlob_checkByte.restype = ctypes.c_byte
+frontierRSBlob_checkByte.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getInt
+frontierRSBlob_getInt = libfc.frontierRSBlob_getInt
+frontierRSBlob_getInt.restype = ctypes.c_int
+frontierRSBlob_getInt.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getLong
+frontierRSBlob_getLong = libfc.frontierRSBlob_getLong
+frontierRSBlob_getLong.restype = ctypes.c_longlong
+frontierRSBlob_getLong.argtypes= [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getDouble
+frontierRSBlob_getDouble = libfc.frontierRSBlob_getDouble
+frontierRSBlob_getDouble.restype = ctypes.c_double
+frontierRSBlob_getDouble.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getFloat
+frontierRSBlob_getFloat = libfc.frontierRSBlob_getFloat
+frontierRSBlob_getFloat.restype = ctypes.c_float
+frontierRSBlob_getFloat.argtypes = [ctypes.c_void_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getByteArray
+frontierRSBlob_getByteArray = libfc.frontierRSBlob_getByteArray
+frontierRSBlob_getByteArray.restype = ctypes.c_void_p #????
+frontierRSBlob_getByteArray.argtypes = [ctypes.c_void_p, ctypes.c_uint, ctypes.POINTER(ctypes.c_int)]
+
+#frontier.h frontierRSBlob_getRecNum
+frontierRSBlob_getRecNum = libfc.frontierRSBlob_getRecNum
+frontierRSBlob_getRecNum.restype = ctypes.c_uint
+frontierRSBlob_getRecNum.argtypes = [ctypes.c_void_p]
+
+#frontier.h frontierRSBlob_getPos
+frontierRSBlob_getPos = libfc.frontierRSBlob_getPos
+frontierRSBlob_getPos.restype = ctypes.c_uint
+frontierRSBlob_getPos.argtypes = [ctypes.c_void_p]
+
+#frontier.h frontierRSBlob_getSize
+frontierRSBlob_getSize = libfc.frontierRSBlob_getSize
+frontierRSBlob_getSize.restype = ctypes.c_uint
+frontierRSBlob_getSize.argtypes = [ctypes.c_void_p]
+
+#frontier.h frontierRSBlob_payload_error
+frontierRSBlob_payload_error = libfc.frontierRSBlob_payload_error
+frontierRSBlob_payload_error.restype = ctypes.c_int
+frontierRSBlob_payload_error.argtypes = [ctypes.c_void_p]
+
+#frontier.h frontierRSBlob_payload_msg
+frontierRSBlob_payload_msg = libfc.frontierRSBlob_payload_msg
+frontierRSBlob_payload_msg.restype = ctypes.c_char_p
+frontierRSBlob_payload_msg.argtypes = [ctypes.c_void_p]
+
+#frontier.h frontier_getRawData
+frontier_getRawData = libfc.frontier_getRawData
+frontier_getRawData.restype = ctypes.c_ulong
+frontier_getRawData.argtypes = [ctypes.c_ulong,ctypes.c_char_p]
+
+
+#frontier.h frontier_creatChannel
+frontier_createChannel = libfc.frontier_createChannel
+frontier_createChannel.restype = ctypes.c_ulong
+frontier_createChannel.argtype = [ctypes.c_char_p,ctypes.c_char,ctypes.POINTER(ctypes.c_int) ]
+
+#frontier.h frontier_closeChannel
+frontier_closeChannel = libfc.frontier_closeChannel
+frontier_closeChannel.restype = None
+frontier_closeChannel.argtypes = [ctypes.c_ulong]
+
+#frontierConfig.h frontierConfig_get
+frontierConfig_get = libfc.frontierConfig_get
+frontierRSBlob_payload_msg.restype = ctypes.POINTER(FrontierConfig)
+frontierRSBlob_payload_msg.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]
+
+#frontierConfig.h frontierConfig_addServer
+frontierConfig_addServer = libfc.frontierConfig_addServer
+frontierConfig_addServer.restype = ctypes.c_int
+frontierConfig_addServer.argtypes = [ctypes.POINTER(FrontierConfig), ctypes.c_char_p]
+
+#frontier_config.h frontierConfig_addProxy
+frontierConfig_addProxy = libfc.frontierConfig_addProxy
+frontierConfig_addProxy.restype = ctypes.c_int
+frontierConfig_addProxy.argtypes = [ctypes.POINTER(FrontierConfig), [ctypes.c_char_p]
+                                    
+# wrapped functions , objects
+
+class FrontierClientError(Exception):
+    def __init__(self, retcode, message):
+        self.args = (retcode, message)
+                                    
+class FrontierConfig(ctypes.Structure):            
+    _fields_ = [('server', ctypes.c_char_p*FRONTIER_MAX_SERVERN ),('proxy',ctypes.c_char_p*FRONTIER_MAX_PROXYN),('proxyconfig',ctypes.c_char_p*FRONTIER_MAX_PROXYCONFIGN),('server_num',ctypes.c_int),('proxy_num',ctypes.c_int),('proxyconfig_num',ctypes.c_int),('server_cur',ctypes.c_int),('proxy_cur',ctypes.c_int),('proxyconfig_cur',ctypes.c_int),('servers_balanced',ctypes.c_int),('proxies_balanced',ctypes.c_int),('num_backupproxies',ctypes.c_int),('connect_timeout_secs',ctypes.c_int),('read_timeout_secs',ctypes.c_int),('write_timeout_secs',ctypes.c_int),('max_age_secs',ctypes.c_int),('force_reload',ctypes.c_char_p),('freshkey',ctypes.c_char_p),('retrieve_zip_level',ctypes.c_int),('secured',ctypes.c_int),('capath',ctypes.c_char_p),('client_cache_max_result_size',ctypes.c_int),('failover_to_server',ctypes.c_int),('prefer_ip_family',ctypes.c_int)]
+
 def _build_frontierRSBlob_get(f):
     def wrapper(rsb):
         retcode = ctypes.c_int(FRONTIER_OK)
@@ -47,82 +177,7 @@ def _build_frontierRSBlob_get(f):
         return result
     return wrapper
 
-#functions accept any ctypes data instances as arguments and return the default result type specified by the library loader.
-
-# frontier_error.h frontier_getErrorMsg
-frontier_getErrorMsg = libfc.frontier_getErrorMsg
-frontier_getErrorMsg.restype = ctypes.c_char_p 
-
-#frontier.h frontierRSBlob_open
-frontierRSBlob_open = libfc.frontierRSBlob_open
-frontierRSBlob_open.restype = ctypes.c_void_p
-
-#frontier.h frontierRSBlob_close
-frontierRSBlob_close = libfc.frontierRSBlob_close
-frontierRSBlob_close.restype = None
-
-#frontier.h frontierRSBlob_start
-frontierRSBlob_start = libfc.frontierRSBlob_start
-frontierRSBlob_start.restype = None
-
-#frontier.h frontierRSBlob_getByte
-frontierRSBlob_getByte = libfc.frontierRSBlob_getByte
-frontierRSBlob_getByte.restype = ctypes.c_byte
-
-#frontier.h frontierRSBlob_checkByte
-frontierRSBlob_checkByte = libfc.frontierRSBlob_checkByte
-frontierRSBlob_checkByte.restype = ctypes.c_byte
-
-#frontier.h frontierRSBlob_getInt
-frontierRSBlob_getInt = libfc.frontierRSBlob_getInt
-frontierRSBlob_getInt.restype = ctypes.c_int
-
-#frontier.h frontierRSBlob_getLong
-frontierRSBlob_getLong = libfc.frontierRSBlob_getLong
-frontierRSBlob_getLong.restype = ctypes.c_longlong
-
-#frontier.h frontierRSBlob_getDouble
-frontierRSBlob_getDouble = libfc.frontierRSBlob_getDouble
-frontierRSBlob_getDouble.restype = ctypes.c_double
-
-#frontier.h frontierRSBlob_getFloat
-frontierRSBlob_getFloat = libfc.frontierRSBlob_getFloat
-frontierRSBlob_getFloat.restype = ctypes.c_float
-
-#frontier.h frontierRSBlob_getByteArray
-frontierRSBlob_getByteArray = libfc.frontierRSBlob_getByteArray
-frontierRSBlob_getByteArray.restype = ctypes.c_void_p
-
-#frontier.h frontierRSBlob_getRecNum
-frontierRSBlob_getRecNum = libfc.frontierRSBlob_getRecNum
-frontierRSBlob_getRecNum.restype = ctypes.c_uint
-
-#frontier.h frontierRSBlob_getPos
-frontierRSBlob_getPos = libfc.frontierRSBlob_getPos
-frontierRSBlob_getPos.restype = ctypes.c_uint
-
-#frontier.h frontierRSBlob_getSize
-frontierRSBlob_getSize = libfc.frontierRSBlob_getSize
-frontierRSBlob_getSize.restype = ctypes.c_uint
-
-#frontier.h frontierRSBlob_payload_error
-frontierRSBlob_payload_error = libfc.frontierRSBlob_payload_error
-frontierRSBlob_payload_error.restype = ctypes.c_int
-
-#frontier.h frontierRSBlob_payload_msg
-frontierRSBlob_payload_msg = libfc.frontierRSBlob_payload_msg
-frontierRSBlob_payload_msg.restype = ctypes.c_char_p
-
-# wrapped functions , objects
-
-class FrontierConfig(ctypes.Structure):            
-    _fields_ = [('server', ctypes.c_char_p*FRONTIER_MAX_SERVERN ),('proxy',ctypes.c_char_p*FRONTIER_MAX_PROXYN),('proxyconfig',ctypes.c_char_p*FRONTIER_MAX_PROXYCONFIGN),('server_num',ctypes.c_int),('proxy_num',ctypes.c_int),('proxyconfig_num',ctypes.c_int),('server_cur',ctypes.c_int),('proxy_cur',ctypes.c_int),('proxyconfig_cur',ctypes.c_int),('servers_balanced',ctypes.c_int),('proxies_balanced',ctypes.c_int),('num_backupproxies',ctypes.c_int),('connect_timeout_secs',ctypes.c_int),('read_timeout_secs',ctypes.c_int),('write_timeout_secs',ctypes.c_int),('max_age_secs',ctypes.c_int),('force_reload',ctypes.c_char_p),('freshkey',ctypes.c_char_p),('retrieve_zip_level',ctypes.c_int),('secured',ctypes.c_int),('capath',ctypes.c_char_p),('client_cache_max_result_size',ctypes.c_int),('failover_to_server',ctypes.c_int),('prefer_ip_family',ctypes.c_int)]
-    
-class FrontierClientError(Exception):
-    def __init__(self, retcode, message):
-        self.args = (retcode, message)
-    
-def frontier_createChannel(serverURL = None, proxyURL = None):
+def py_frontier_createChannel(serverURL = None, proxyURL = None):
     logger.debug('frontier_client.frontier_createChannel(serverURL = %s, proxyURL = %s)', repr(serverURL), repr(proxyURL) )
     retcode = ctypes.c_int(FRONTIER_OK)
     channel = libfc.frontier_createChannel(serverURL, proxyURL, ctypes.byref(retcode))
@@ -132,18 +187,8 @@ def frontier_createChannel(serverURL = None, proxyURL = None):
     logger.debug('frontier_client.frontier_createChannel(serverURL = %s, proxyURL = %s) = %s', repr(serverURL), repr(proxyURL), channel)
     return channel
 
-#frontier_config.h frontierConfig_get
-frontierConfig_get = libfc.frontierConfig_get
-frontierRSBlob_payload_msg.restype = ctypes.POINTER(FrontierConfig)
 
-frontierConfig_addServer = libfc.frontierConfig_addServer
-frontierConfig_addServer.restype = ctypes.c_int
-
-frontierConfig_addProxy = libfc.frontierConfig_addProxy
-frontierConfig_addProxy.restype = ctypes.c_int
-
-
-def frontierConfig_addServer(fconfig, serverURL):
+def py_frontierConfig_addServer(fconfig, serverURL):
     retcode = ctypes.c_int(FRONTIER_OK)
     pos = libfc.frontierConfig_addServer( ctype.byref(fconfig), serverURL)
     retcode = retcode.value
@@ -152,7 +197,7 @@ def frontierConfig_addServer(fconfig, serverURL):
     logger.debug('frontier_client.frontierConfig_addServer(serverURL = %s) = %s', repr(serverURL), pos)
     return pos
 
-def frontierConfig_addProxy(fconfig, proxyURL):
+def py_frontierConfig_addProxy(fconfig, proxyURL):
     retcode = ctypes.c_int(FRONTIER_OK)
     pos = libfc.frontierConfig_addProxy( ctype.byref(fconfig), proxyURL)
     retcode = retcode.value
@@ -179,18 +224,18 @@ def frontierConfig_addProxy(fconfig, proxyURL):
 #    logger.debug('frontier_client.frontier_createChannel2(serverURLs = %s, proxyURLs = %s) = %s', repr(serverURLs), repr(proxyURLs), channel)
 #    return channel
 
-def frontier_closeChannel(channel):
-    logger.debug('frontier_client.frontier_closeChannel(channel = %s)', repr(channel) )
+def py_frontier_closeChannel(channel):
+    logger.debug('py_frontier_closeChannel(channel = %s)', repr(channel) )
     libfc.frontier_closeChannel(channel)
 
-def frontier_getRawData(channel, uri):
-    logger.debug('frontier_client.frontier_getRawData(channel = %s, uri = %s)', repr(channel), repr(uri) )
+def py_frontier_getRawData(channel, uri):
+    logger.debug('py_frontier_getRawData(channel = %s, uri = %s)', repr(channel), repr(uri) )
     retcode = libfc.frontier_getRawData(channel, uri)
     if retcode != FRONTIER_OK:
         raise FrontierClientError(retcode, libfc.frontier_getErrorMsg())
 
-def frontierRSBlob_open(channel, oldrs, n):
-    logger.debug('frontier_client.frontierRSBlob_open(channel = %s, oldrs = %s, n = %s)', channel, oldrs, n )
+def py_frontierRSBlob_open(channel, oldrs, n):
+    logger.debug('py_frontierRSBlob_open(channel = %s, oldrs = %s, n = %s)', channel, oldrs, n )
     retcode = ctypes.c_int(FRONTIER_OK)
     rs = libfc.frontierRSBlob_open(channel, oldrs, n, ctypes.byref(retcode) )
     retcode =  retcode.value
@@ -201,24 +246,24 @@ def frontierRSBlob_open(channel, oldrs, n):
         raise FrontierClientError(retcode, libfc.frontierRSBlob_payload_msg(rs))
     return rs
 
-def frontierRSBlob_close(rs):
-    logger.debug('frontier_client.frontierRSBlob_close(rs = %s)', rs)
+def py_frontierRSBlob_close(rs):
+    logger.debug('py_frontierRSBlob_close(rs = %s)', rs)
     retcode = ctypes.c_int(FRONTIER_OK)
     libfc.frontierRSBlob_close(rs, ctypes.byref(retcode) )
     retcode = retcode.value
     if retcode != FRONTIER_OK:
         raise FrontierClientError(retcode, libfc.frontier_getErrorMsg())
 
-frontierRSBlob_getByte = _build_frontierRSBlob_get(libfc.frontierRSBlob_getByte)
-frontierRSBlob_checkByte = _build_frontierRSBlob_get(libfc.frontierRSBlob_checkByte)
-frontierRSBlob_getInt = _build_frontierRSBlob_get(libfc.frontierRSBlob_getInt)
-frontierRSBlob_getLong = _build_frontierRSBlob_get(libfc.frontierRSBlob_getLong)
-frontierRSBlob_getDouble = _build_frontierRSBlob_get(libfc.frontierRSBlob_getDouble)
-frontierRSBlob_getFloat = _build_frontierRSBlob_get(libfc.frontierRSBlob_getFloat)
+py_frontierRSBlob_getByte = _build_frontierRSBlob_get(libfc.frontierRSBlob_getByte)
+py_frontierRSBlob_checkByte = _build_frontierRSBlob_get(libfc.frontierRSBlob_checkByte)
+py_frontierRSBlob_getInt = _build_frontierRSBlob_get(libfc.frontierRSBlob_getInt)
+py_frontierRSBlob_getLong = _build_frontierRSBlob_get(libfc.frontierRSBlob_getLong)
+py_frontierRSBlob_getDouble = _build_frontierRSBlob_get(libfc.frontierRSBlob_getDouble)
+py_frontierRSBlob_getFloat = _build_frontierRSBlob_get(libfc.frontierRSBlob_getFloat)
 
 
-def frontierRSBlob_getByteArray(rs):
-    buflen = frontierRSBlob_getInt(rs)
+def py_frontierRSBlob_getByteArray(rs):
+    buflen = py_frontierRSBlob_getInt(rs)
     retcode = ctypes.c_int(FRONTIER_OK)
     buf = libfc.frontierRSBlob_getByteArray(rs, buflen, ctypes.byref(retcode))
     retcode = retcode.value
@@ -227,37 +272,38 @@ def frontierRSBlob_getByteArray(rs):
     s = ctypes.string_at(buf, buflen)
     return s
 
-def frontierRSBlob_getRecNum(rs):
+def py_frontierRSBlob_getRecNum(rs):
     '''
     get number of returned records
     '''
-    logger.debug('frontier_client.frontierRSBlob_getRecNum(rs = %s)', rs )
+    logger.debug('py_frontierRSBlob_getRecNum(rs = %s)', rs )
     return libfc.frontierRSBlob_getRecNum(rs)
 
-def frontier_mem_free(address):
+def py_frontier_mem_free(address):
     '''
     C.in_dll(dll, name) access a C ubstabce in a dll 
     '''
     ctypes.CFUNCTYPE(None, ctypes.c_void_p)(ctypes.c_void_p.in_dll(libfc, 'frontier_mem_free').value)(address)
 
-def fn_gzip_str2urlenc(string):
+def py_fn_gzip_str2urlenc(string):
     '''
     gzip string then 64base encode to ascii 
     fn-zlib.c
     '''
-    logger.debug('frontier_client.fn_gzip_str2urlenc(string = %s)', repr(string))
+    logger.debug('py_fn_gzip_str2urlenc(string = %s)', repr(string))
     buf = ctypes.c_void_p()
     buflen = libfc.fn_gzip_str2urlenc(string, len(string), ctypes.byref(buf))
     if buflen < 0 :
         raise FrontierClientError(None, 'Impossible to encode.')
     s = ctypes.string_at(buf.value, buflen)
-    frontier_mem_free(buf.value)
+    py_frontier_mem_free(buf.value)
     return s
     
-def frontier_init():
-    logger.debug('frontier_client.frontier_init()')
+def py_frontier_init():
+    logger.debug('py_frontier_init()')
     retcode = libfc.frontier_init(None, None)
     logger.debug('retcode '+str(retcode))
     if retcode != FRONTIER_OK:
         raise FrontierClientError(retcode, libfc.frontier_getErrorMsg())
-frontier_init()
+
+py_frontier_init()
