ad69b8d799566f8a85ed4cce427fc905d9d489a5
Zhen Xie <zhen.xie@cern.ch>
2023-09-13 20:54:18 +0200

    add py_modules argument

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 
 setup(
     name = 'sqlalchemy_frontier',
-    version = '2.8.19',
+    version = '2.10.2',
     description = 'sqlalchemy driver for frontier_client',    
-    data_files = [('connectors',['connectors/frontier.py']),('dialects/oracle',['dialects/oracle/frontier_client_ctypes.py','dialects/oracle/frontier_dbapi.py','dialects/oracle/frontier.py'])]
+    data_files = [('connectors',['connectors/frontier.py']),('dialects/oracle',['dialects/oracle/frontier_client_ctypes.py','dialects/oracle/frontier_dbapi.py','dialects/oracle/frontier.py'])],
+    py_modules = []
 )
 
